5a2ff0cf6a8dcc9f879216355ec9b4a7d87cafa3
RafaelRufino <rafaelrufino.52@gmail.com>
2021-12-28 01:36:18 -0300

    criando components Carros

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -3,8 +3,30 @@ import { StatusBar } from "react-native";
 import Logo from "../../assets/logo.svg";
 import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
 import { Container, Header, TotalCars, HeaderContent } from "./styles";
+import { Car } from "../../components/Car";
 
 export function Home() {
+  const carDataOne = {
+    brand: "AUDI",
+    name: "5 Coupé",
+    rent: {
+      period: "AO DIA",
+      price: 120,
+    },
+    thumbnail:
+      "https://png.monster/wp-content/uploads/2020/11/2018-audi-rs5-4wd-coupe-angular-front-5039562b.png",
+  };
+  const carDataTwo = {
+    brand: "Porsche",
+    name: "Panamera",
+    rent: {
+      period: "AO DIA",
+      price: 120,
+    },
+    thumbnail:
+      "https://production.autoforce.com/uploads/version/profile_image/3188/model_main_comprar-tiptronic_87272c1ff1.png",
+  };
+
   return (
     <Container>
       <StatusBar
@@ -18,6 +40,8 @@ export function Home() {
           <TotalCars>Total de 12 carros</TotalCars>
         </HeaderContent>
       </Header>
+      <Car data={carDataOne} />
+      <Car data={carDataTwo} />
     </Container>
   );
 }
