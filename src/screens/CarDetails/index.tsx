22bc4e806ef504983da07ee0725a07549cfea1ff
RafaelRufino <rafaelrufino.52@gmail.com>
2021-12-28 22:40:29 -0300

    concluindo component de butao

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { Accessory } from "../../components/Accessory";
 import { BackButton } from "../../components/BackButton";
 import { ImageSlider } from "../../components/ImageSlider";
-
+import speedSvg from "../../assets/speed.svg";
+import accelerationSvg from "../../assets/acceleration.svg";
+import forceSvg from "../../assets/force.svg";
+import gasolineSvg from "../../assets/gasoline.svg";
+import exchangeSvg from "../../assets/exchange.svg";
+import peopleSvg from "../../assets/people.svg";
 import {
   Container,
   Header,
@@ -15,7 +21,10 @@ import {
   Price,
   Datails,
   About,
+  Acessories,
+  Footer,
 } from "./styles";
+import { Button } from "../../components/Button";
 
 export function CarDetails() {
   return (
@@ -43,12 +52,23 @@ export function CarDetails() {
             <Price>R$ 500</Price>
           </Rent>
         </Datails>
+        <Acessories>
+          <Accessory name="380km/h" icon={speedSvg} />
+          <Accessory name="3.2s" icon={accelerationSvg} />
+          <Accessory name="800 HP" icon={forceSvg} />
+          <Accessory name="Gasolina" icon={gasolineSvg} />
+          <Accessory name="Auto" icon={exchangeSvg} />
+          <Accessory name="2 pessoas" icon={peopleSvg} />
+        </Acessories>
         <About>
           Este é automóvel desportivo. Surgiu do lendário touro lide indultado
           na número praça Real maestranza de servilla. è um beissimo arro para
           quem gosta de acelerar.
         </About>
       </Content>
+      <Footer>
+        <Button title="Confirma" />
+      </Footer>
     </Container>
   );
 }
