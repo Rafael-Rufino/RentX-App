ab6beb1368855ac119e9d2720ed75def47228389
RafaelRufino <rafaelrufino.52@gmail.com>
2022-01-05 11:53:25 -0300

    finalizando iinterface principal

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -2,19 +2,31 @@ import React, { useEffect, useState } from "react";
 import { StatusBar } from "react-native";
 import Logo from "../../assets/logo.svg";
 import { RFValue } from "react-native-responsive-fontsize";
-import { Container, Header, TotalCars, HeaderContent, CarList } from "./styles";
 import { Car } from "../../components/Car";
 import { Load } from "../../components/Load";
 import { useNavigation } from "@react-navigation/native";
 import api from "../../services/api";
 import { CarDTO } from "../../dtos/CarDTO";
+import { Ionicons } from "@expo/vector-icons";
+import {
+  Container,
+  Header,
+  TotalCars,
+  HeaderContent,
+  CarList,
+  MyCarsButton,
+} from "./styles";
+import { useTheme } from "styled-components";
 export function Home() {
   const [cars, setCars] = useState<CarDTO[]>([]);
   const [isLoading, setLoading] = useState(true);
   const navigation = useNavigation();
-
+  const theme = useTheme();
   function handleCarDetails(car: CarDTO) {
     navigation.navigate("CarDetails", { car });
+  }
+  function handleOpenMyCars() {
+    navigation.navigate("MyCars");
   }
   useEffect(() => {
     async function fetchCars() {
@@ -54,6 +66,9 @@ export function Home() {
           )}
         />
       )}
+      <MyCarsButton onPress={handleOpenMyCars}>
+        <Ionicons name="ios-car-sport" size={32} color={theme.colors.shape} />
+      </MyCarsButton>
     </Container>
   );
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -3,6 +3,7 @@ import theme from "../../global/styles/theme";
 import { FlatList } from "react-native";
 import { RFValue } from "react-native-responsive-fontsize";
 import { CarDTO } from "../../dtos/CarDTO";
+import { RectButton } from "react-native-gesture-handler";
 export const Container = styled.View`
   flex: 1;
   background-color: ${({ theme }) => theme.colors.background_primary};
@@ -33,3 +34,15 @@ export const CarList = styled(FlatList as new () => FlatList<CarDTO>).attrs({
   },
   showsVerticalScrollIndicator: false,
 })``;
+
+export const MyCarsButton = styled(RectButton)`
+  width: 60px;
+  height: 60px;
+  justify-content: center;
+  align-items: center;
+  border-radius: 30px;
+  position: absolute;
+  bottom: 13px;
+  right: 22px;
+  background-color: ${({ theme }) => theme.colors.main};
+`;
